cc71dafd781683eb90fa14e7058d7add09cd8354
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-24 22:23:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,29 @@
-class Student 
+class Student < User
+@knowledge = ["a String is a type of data in Ruby",
+    "programming is hard, but it's worth it",
+    "javascript async web request",
+    "Ruby method call definition",
+    "object oriented dog cat class instance",
+    "class method class variable instance method instance variable",
+     "programming computers hacking learning terminal",
+     "bash Ruby rvm update certs"]
+
+#attr_accessor :first_name, :last_name, :user, :teacher, :student
+
+  def initialize
+  @first_name = first_name
+  @last_name = last_name
+  @knowledge = []
+  end
+
+  def learn#( string: "Ruby framework Rails gem bundle update")
+  @knowledge << "Ruby framework Rails gem bundle update"
+  end
+
+  def knowledge
+  #   puts "sample() method form : #{KNOWLEDGE.sample(9)}\n\n"
+    @knowledge < Teacher.teach
+    @knowledge
+   end
 
 end
